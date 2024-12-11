71acfc864328cf608968971725953e1efee041ba
ByJuix <julian@toast24.de>
2024-12-11 11:19:19 +0100

    namen und so

--- index.php
+++ index.php
@@ -8,14 +8,17 @@ class Charakter {
     private $intelligence;
     private $color;
 
+    // Possible names array
+    private $possibleNames = ["Cyanis der Schimmernde", "Kuphero der Glühende", "Rubinia Flammenklinge", "Azubios der Garagenfeger", "Ambera Goldhand", "Vermilios Stahlseele", "Bronzora die Mächtige", "Zinnox der Verschlagene", "Smargant der Weise", "Alabastea der Erhabene", "Saphiriel Sturmbrecher", "Ochros Kupferflamme", "Chalybeus der Unverwüstliche", "Verdantus Blattläufer", "Aurenix der Glanzvolle", "Carminelle Schattenruferin", "Cobalta Nachtseele", "Malach der Grüne Hüter", "Zirkon Flammensucher", "Titanora die Ewige", "Feldmannius der Göttliche"];
+
     // Konstruktor
-    public function __construct($name, $health, $strength, $dexterity, $intelligence) {
-        $this->color = rand(1,100);
-        $this->name = $name;
-        $this->health = $health * ((100+ $this->color)/100);
-        $this->strength = $strength * ((100+ $this->color)/100);
-        $this->dexterity = $dexterity * ((100+ $this->color)/100);
-        $this->intelligence = $intelligence * ((100+ $this->color)/100);
+    public function __construct($name = null, $health, $strength, $dexterity, $intelligence) {
+        $this->name == "Feldmannius der Göttliche" ? $this->color = rand(1, 100) : $this->color = rand(100, 500);
+        $this->name = $name ?? $this->possibleNames[array_rand($this->possibleNames)];
+        $this->health = $health * ((100 + $this->color) * 0.01);
+        $this->strength = round($strength * ((100 + $this->color) * 0.01), 0);
+        $this->dexterity = round($dexterity * ((100 + $this->color) * 0.01), 0);
+        $this->intelligence = round($intelligence * ((100 + $this->color) * 0.01), 0);
     }
 
     // Destruktor
@@ -73,18 +76,18 @@ class Charakter {
         $this->color = $color;
     }
 
-    private function Attack ($Enemy){
+    private function Attack($Enemy) {
         $Enemy->Defend($this->dexterity, $this->getStrength());
     }
-    
-    private function Defend ($EnemyDex, $Damage){
-        $DamageTaken = $EnemyDex/$this->dexterity *$Damage;
+
+    private function Defend($EnemyDex, $Damage) {
+        $DamageTaken = $EnemyDex / $this->dexterity * $Damage;
         $this->setHealth($this->getHealth() - $DamageTaken);
     }
 }
 
 // Beispiel zur Nutzung der Klasse
-$charakter = new Charakter("Held", 100, 100, 20, 15);
+$charakter = new Charakter(null, 1000, 37, 20, 15);
 echo "Name: " . $charakter->getName() . "<br>";
 echo "Health: " . $charakter->getHealth() . "<br>";
 echo "Strength: " . $charakter->getStrength() . "<br>";
