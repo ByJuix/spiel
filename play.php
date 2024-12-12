9afd462127f6dfade11aa5ef53f3a34e094eaa2f
Pinguin4Nation <motymoteistnice@gmail.com>
2024-12-12 13:29:52 +0100

    controls

---
+++ play.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <script src="game.js"></script>
+    </head>
+    <body>
+        <h1>eContriver</h1>
+        <h2>JavaScript WASD Game</h2>
+        <img src="dot.png"/>
+        <div id="canvas">
+            <canvas id="game" width="1040" height="640"></canvas>
+        </div>
+    </body>
+</html>
